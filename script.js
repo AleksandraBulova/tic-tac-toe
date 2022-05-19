15e72f89fc771cddb7dd5b61eb39995b57e2c3e2
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2022-05-19 12:17:30 +0400

    feat: implement local storage for 'Statistics'

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   document.addEventListener('click', function(event) {
     if( event.target.className !== 'button__settings' && event.target.className !== 'settings active' &&
-    event.target.className !== 'restart settings_text' && event.target.className !== 'change settings_text') {
+    event.target.className !== 'restart settings_text' && event.target.className !== 'change settings_text' && 
+    event.target.className !== 'statistics-game settings_text') {
       settings.classList.remove('active')
     }
   })
@@ -33,11 +34,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
   ] 
 
   let arrStatistics = []
+  let historyLocalStorage = JSON.parse(localStorage.getItem('win'))
+  
+  for(let i = 0; i < historyLocalStorage.length; i++){
+    arrStatistics.push(historyLocalStorage[i])
+    if (historyLocalStorage[i] === 'X'){
+      document.getElementById(`item-${[i]}`).innerHTML = 'Player win : X '
+    } 
+    if (historyLocalStorage[i] === '0') {
+      document.getElementById(`item-${[i]}`).innerHTML = 'Player win : 0 '
+    }
+    if (historyLocalStorage[i] === 'Draw') {
+      document.getElementById(`item-${[i]}`).innerHTML = 'Draw game'
+    }
+  }
+
 
   for (let i = 0; i < cell.length; i++) {
     cell[i].addEventListener('click', function(event) {
-      count++
-
       const cellElement = event.composedPath().find(el => el.className === 'playing-field_cell')
       const cellId = event.target.id
       const fillArr = () => {
@@ -60,60 +74,112 @@ document.addEventListener("DOMContentLoaded", function(event) {
         numberMoves.innerHTML = `Number of moves: ${count}`
       }
 
+      const historyStatistics = () => {
+        if(arrStatistics.length < 10){
+          thisX === true ? arrStatistics.push('0') : arrStatistics.push('X')
+        } else {
+          arrStatistics.shift()
+          thisX === true ? arrStatistics.push('0') : arrStatistics.push('X')
+        }
+        localStorage.setItem('win', JSON.stringify(arrStatistics))
+      }
+
+      const fillStatistics = () => {
+        for(let k = 0; k < arrStatistics.length; k++) {
+          if (arrStatistics[k] === 'X'){
+            document.getElementById(`item-${[k]}`).innerHTML = 'Player win : X '
+          } 
+          if (arrStatistics[k] === '0') {
+            document.getElementById(`item-${[k]}`).innerHTML = 'Player win : 0 '
+          }
+          if (arrStatistics[k] === 'Draw') {
+            document.getElementById(`item-${[k]}`).innerHTML = 'Draw game'
+          }
+        }
+      }
+
       const win = document.querySelector('.window-win')
 
       const victoryCheck = () => {
         if(arrWin[0][0] === arrWin[0][1] && arrWin[0][1] === arrWin[0][2] && arrWin[0][0] && arrWin[0][1] && arrWin[0][2] ) {
           textWindowWin()
+          historyStatistics()
+          fillStatistics()
           return win.classList.toggle('active')
         }
         if(arrWin[1][0] === arrWin[1][1] && arrWin[1][1] === arrWin[1][2] && arrWin[1][0] && arrWin[1][1] && arrWin[1][2]) {
           textWindowWin()
+          historyStatistics()
+          fillStatistics()
           return win.classList.toggle('active')
         }
         if(arrWin[2][0] === arrWin[2][1] && arrWin[2][1] === arrWin[2][2] && arrWin[2][0] && arrWin[2][1] && arrWin[2][2]) {
           textWindowWin()
+          historyStatistics()
+          fillStatistics()
           return win.classList.toggle('active')
         }
         if(arrWin[0][0] === arrWin[1][0] && arrWin[1][0] === arrWin[2][0] && arrWin[0][0] && arrWin[1][0] && arrWin[2][0] ) {
           textWindowWin()
+          historyStatistics()
+          fillStatistics()
           return win.classList.toggle('active')
         }
         if(arrWin[0][1] === arrWin[1][1] && arrWin[1][1] === arrWin[2][1] && arrWin[0][1] && arrWin[1][1] && arrWin[2][1]) {
           textWindowWin()
+          historyStatistics()
+          fillStatistics()
           return win.classList.toggle('active')
         }
         if(arrWin[0][2] === arrWin[1][2] && arrWin[1][2] === arrWin[2][2] && arrWin[0][2] && arrWin[1][2] && arrWin[2][2]) {
           textWindowWin()
+          historyStatistics()
+          fillStatistics()
           return win.classList.toggle('active')
         }
         if(arrWin[0][0] === arrWin[1][1] && arrWin[1][1] === arrWin[2][2] && arrWin[0][0] && arrWin[1][1] && arrWin[2][2]) {
           textWindowWin()
+          historyStatistics()
+          fillStatistics()
           return win.classList.toggle('active')
         }
         if(arrWin[0][2] === arrWin[1][1] && arrWin[1][1]  === arrWin[2][0] && arrWin[0][2] && arrWin[1][1] && arrWin[2][0]) {
           textWindowWin()
+          historyStatistics()
+          fillStatistics()
           return win.classList.toggle('active')
         }
         if(arrWin[0][0] && arrWin[0][1] && arrWin[0][2] && arrWin[1][0] && arrWin[1][1] && arrWin[1][2] && 
           arrWin[2][0] && arrWin[2][1] && arrWin[2][2]) {
           playerWin.innerHTML = `Draw!` 
           numberMoves.innerHTML = `Number of moves: ${count}`
+          if(arrStatistics.length < 10){
+            thisX === true ? arrStatistics.push('Draw') : arrStatistics.push('Draw')
+          } else {
+            arrStatistics.shift()
+            thisX === true ? arrStatistics.push('Draw') : arrStatistics.push('Draw')
+          }
+          localStorage.setItem('win', JSON.stringify(arrStatistics))
+          fillStatistics()
           return win.classList.toggle('active')
         }
       }
 
       if(cellElement.innerHTML === '' && thisX === true) {
+        count++
         cellElement.innerHTML = 'X'
         thisX = !thisX
         fillArr()
         victoryCheck()
       } else if(cellElement.innerHTML === '') {
+        count++
         cellElement.innerHTML = 'O'
         thisX = !thisX
         fillArr()
         victoryCheck()
       }
+
+      console.log(arrStatistics, localStorage.getItem('win'))
 
       const newGame = () =>{
         thisX = true
@@ -152,4 +218,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   })
 
+  const statisticsButton = document.querySelector('.statistics-game')
+  const statisticsGame = document.querySelector('.statistics')
+  const statisticsClose = document.querySelector('.statistics__close')
+
+  statisticsButton.addEventListener('click', function() {
+    statisticsGame.classList.toggle('active')
+  })
+
+  statisticsClose.addEventListener('click', function() {
+    statisticsGame.classList.remove('active')
+  })
+  
 })
